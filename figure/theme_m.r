4d5ef91df501bf8f62b5704a2aef92b3c836e04a
schifzt <tagashira.fumito@gmail.com>
2020-04-12 15:18:11 +0900

    update theme_m.r

--- figure/theme_m.r
+++ figure/theme_m.r
@@ -10,13 +10,14 @@
 # 	  scale_y_continuous(sec.axis = dup_axis())
 
 theme_m <- function() {
+	text_size <- 14
 	return(
 		theme_linedraw() +
 		theme(
 			# Inward axis(t,b,l,r)
 			axis.ticks.length = unit(-0.25, "cm"), 
-  			axis.text.x.bottom = element_text(size = 16, margin = margin(t = 0.35, unit = "cm")),
-  			axis.text.y.left = element_text(size = 16, margin = margin(r = 0.35, unit = "cm")),
+  			axis.text.x.bottom = element_text(size = text_size, margin = margin(t = 0.35, unit = "cm")),
+  			axis.text.y.left = element_text(size = text_size, margin = margin(r = 0.35, unit = "cm")),
 			
 			# Remove secondary axis label
   			axis.text.x.top = element_text(size = 0, margin = margin(b = 0.35, unit = "cm")),
@@ -27,15 +28,32 @@ theme_m <- function() {
 			axis.ticks = element_line(size = 0.5),
 
 			# ==============================
+	
+			# Legend text
+			legend.text = element_text(size = text_size),
+			legend.title = element_blank(),
 
 			# Legend float position
-			legend.position = c(.05, .99),
+			legend.position = c(.00, .95),
 			legend.justification = c("left", "top"),
-			legend.box.just = "left",
+			legend.box.just = "top",
 			legend.margin = margin(6, 6, 6, 6),
+			legend.box = "horizontal",
 			
-			# Legened color
-  			legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour="black"),
+			# Legened color(white + frame)
+  			# legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour="black"),
+
+			# Legened color(transparent)
+  			legend.background = element_rect(colour = "transparent", fill = NA),
+
+			# ==============================
+
+			# Remove grid line (optional)
+  			panel.grid = element_blank(),
+
+			# grid line (dot)
+  			# panel.grid = element_line(size = 2, linetype = "1F", color = "black"),
+  			# panel.grid.minor = element_blank(),
 
 			# ==============================
 
@@ -43,8 +61,7 @@ theme_m <- function() {
 			axis.title.x = element_blank(), 
 			axis.title.y = element_blank(),
 
-			# Remove grid line (optional)
-  			# panel.grid = element_blank(),
+
 		)
 	)
 }
